d1e2f75a15d7fba278eadc0fd300e0b3c626ba57
samuel <samuel.slavik@protonmail.com>
2025-07-09 18:33:53 +0200

    Add storybook

---
+++ packages/dummy/src/component/dummy/__tests__/Dummy.stories.ts
@@ -0,0 +1,15 @@
+import { Dummy } from '../Dummy';
+import { StoryObj } from '@storybook/react';
+
+const meta = {
+    title: 'dummy/Dummy',
+    component: Dummy,
+    args: {
+        dummyText: 'Dummy text',
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
