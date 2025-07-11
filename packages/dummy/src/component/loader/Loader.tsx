b2aebcfcb1fc694cfa9f11e03241f90e5654d4a7
Zelo <lukyy6329@gmail.com>
2025-07-11 21:25:10 +0200

    Add Loader component
    Introduces a reusable Loader React component with customizable loading state, type, color, and className. The component displays a loader or its children based on the loading prop.

---
+++ packages/dummy/src/component/loader/Loader.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./loader.css";
+
+export interface LoaderProps {
+  loading?: boolean;
+  type?: string;
+  color?: string;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const Loader: React.FC<LoaderProps> = ({
+  loading = true,
+  type = "spinner",
+  color,
+  children,
+  className = "",
+}) => {
+  const style: React.CSSProperties = color
+    ? ({ "--color": color } as React.CSSProperties)
+    : {};
+
+  return (
+    <div>
+      {loading ? (
+        <div
+          className={className + " " + "ms-loader " + "ms-loader--" + type}
+          style={style}
+        ></div>
+      ) : (
+        children
+      )}
+    </div>
+  );
+};
+
+export default Loader;
