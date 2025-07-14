791244603c52cb365fe7c4298e65b357da07126e
samuel <samuel.slavik@protonmail.com>
2025-07-14 20:21:16 +0200

    Refactor building process

--- packages/dummy/rollup.config.js
+++ packages/dummy/rollup.config.js
@@ -1,19 +1,3 @@
-import dts from 'rollup-plugin-dts';
-import { createBaseConfig } from '../../rollup.config.base';
+import { createBaseConfig, createTypeConfig } from '../../rollup.config.base';
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const packageJson = require('./package.json');
-
-export default [
-    createBaseConfig(),
-    {
-        input: 'src/main.ts',
-        output: [
-            {
-                file: packageJson.types,
-            },
-        ],
-        plugins: [dts.default()],
-        external: [/\.scss/],
-    },
-];
+export default [createBaseConfig(), createTypeConfig()];
--- rollup.config.base.js
+++ rollup.config.base.js
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
 
 export const createBaseConfig = () => {
     return {
@@ -32,10 +33,27 @@ export const createBaseConfig = () => {
                         src: 'src/**/*.scss',
                         dest: 'dist',
                     },
+                    {
+                        src: 'src/**/__tests__/*.stories.@(js|jsx|ts|tsx)',
+                        dest: 'dist',
+                    },
                 ],
                 flatten: false,
             }),
         ],
         external: ['react', 'react-dom'],
+    };
+};
+
+export const createTypeConfig = () => {
+    return {
+        input: 'src/main.ts',
+        output: [
+            {
+                file: 'dist/index.d.ts',
+            },
+        ],
+        plugins: [dts.default()],
+        external: [/\.scss/, /\.stories\.(js|jsx|ts|tsx|mdx)?$/],
     };
 };
