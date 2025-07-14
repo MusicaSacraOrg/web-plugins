43e00fab8fd07809fd06efe6a47b060e3fc7204a
Miroslav <mirkobasista@gmail.com>
2025-07-14 12:02:35 +0200

    feat(#WPLG-4 layout): add LayoutBasic component stories for Storybook

--- packages/layout/src/layouts/layoutBasic/__tests__/LayoutBasic.stories.tsx
+++ packages/layout/src/layouts/layoutBasic/__tests__/LayoutBasic.stories.tsx
@@ -1,11 +1,17 @@
+import { Children } from 'react';
 import { LayoutBasic } from '../LayoutBasic';
 import { StoryObj } from '@storybook/react';
+
+export function Dummy() {
+    return <div style={{ backgroundColor: 'red' }}>{`Your dummy text`}</div>;
+}
 
 const meta = {
     title: 'layout/LayoutBasic',
     component: LayoutBasic,
     args: {
         isPageLayout: true,
+        children: Dummy,
     },
 };
 
