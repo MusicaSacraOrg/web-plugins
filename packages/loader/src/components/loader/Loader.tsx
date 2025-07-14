ab082b9f69ca0a973157ad8659bb1c4ee278a553
Zelo <lukyy6329@gmail.com>
2025-07-14 22:29:05 +0200

    Add loader package to packages with React Loader component
    Moved package to packages and renamed css to scss, made one more folder loader in src.

---
+++ packages/loader/src/components/loader/Loader.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./loader.scss";
+
+export interface LoaderProps {
+  loading?: boolean;
+  color?: string;
+  children?: React.ReactNode;
+}
+
+export const Loader: React.FC<LoaderProps> = ({
+  loading = true,
+  color,
+  children,
+}) => {
+  const style: React.CSSProperties = color
+    ? ({ "--color": color } as React.CSSProperties)
+    : {};
+
+  return (
+    <div>
+      {loading ? <div className="ms-loader" style={style}></div> : children}
+    </div>
+  );
+};
+
+export default Loader;
---
+++ packages/loader/src/main.ts
@@ -0,0 +1,1 @@
+import Loader from "./components/loader/Loader";
