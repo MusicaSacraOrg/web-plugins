bb8906b652e651c9180f7db78306dba66cfc1d1c
Zelo <lukyy6329@gmail.com>
2025-07-18 22:06:58 +0200

    Rollback storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,25 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-import path from "path";
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ["../packages/*/src/**/__tests__/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  webpackFinal: async (config) => {
-    config.module?.rules?.push({
-      test: /\.scss$/,
-      include: path.resolve(__dirname, "../packages"),
-      use: ["style-loader", "css-loader", "sass-loader"],
-    });
-    return config;
-  },
+    stories: ['../packages/**/__tests__/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-webpack5-compiler-swc',
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-interactions',
+    ],
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {},
+    },
+    webpackFinal: async (config) => {
+        config.module?.rules?.push({
+            test: /\.scss$/,
+            include: path.resolve(__dirname, '../packages'),
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        });
+        return config;
+    },
 };
 export default config;
