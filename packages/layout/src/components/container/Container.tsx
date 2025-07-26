e9068853f705c79145ccecb05859e609a82d1aa3
Miroslav <mirkobasista@gmail.com>
2025-07-26 16:29:40 +0200

    refactor(#WPLG-4 container): improve Container component className handling with useClassNames hook

--- packages/layout/src/components/container/Container.tsx
+++ packages/layout/src/components/container/Container.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useClassNames } from '@musica-sacra/hooks';
 
 export type ContainerProps = {
     className?: string;
@@ -11,9 +12,15 @@ export function Container({
     children,
     isPageContainer,
 }: ContainerProps) {
+    const cx = useClassNames();
+
     return (
         <div
-            className={`ms-container ${isPageContainer ? 'ms-container--page-container' : ''} ${className}`}
+            className={cx(
+                'ms-container',
+                isPageContainer && 'ms-container--page-container',
+                className
+            )}
         >
             {children}
         </div>
