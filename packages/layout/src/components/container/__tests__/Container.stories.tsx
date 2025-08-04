19ca333871080d9aa8220fa1c9df23009bdf0090
samuel <samuel.slavik@protonmail.com>
2025-08-04 22:16:01 +0200

    Clean Container implementation

--- packages/layout/src/components/container/__tests__/Container.stories.tsx
+++ packages/layout/src/components/container/__tests__/Container.stories.tsx
@@ -6,21 +6,18 @@ import { Container } from '../Container';
 const meta: Meta<typeof Container> = {
     title: '@musica-sacra/Layout/components/Container',
     component: Container,
+    args: {
+        children: <p>Container content</p>,
+    },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    args: {
-        isPageContainer: false,
-        children: <p>Container content</p>,
-    },
-};
+export const Default: Story = {};
 
 export const PageLayoutContainer: Story = {
     args: {
         isPageContainer: true,
-        children: <p>Container content - full page height</p>,
     },
 };
