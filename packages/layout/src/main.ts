9a76750ecac6b3883e2c5ca3d34ca8a977f2c3aa
samuel <samuel.slavik@protonmail.com>
2025-08-04 21:03:07 +0200

    Fix dependencies

--- packages/layout/src/main.ts
+++ packages/layout/src/main.ts
@@ -2,6 +2,5 @@ import { Container } from './components/container/Container';
 
 import { LayoutBasic } from './components/layoutBasic/LayoutBasic';
 import { LayoutWithSidebar } from './components/layoutWithSidebar/LayoutWithSidebar';
-import { TwoSidebarLayout } from './components/layoutWithTwoSidebars/TwoSidebarLayout';
 
-export { Container, LayoutBasic, LayoutWithSidebar, TwoSidebarLayout };
+export { Container, LayoutBasic, LayoutWithSidebar };
