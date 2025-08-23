a7cc2ff0a4eaed446593b2e105ad683c69ed70b2
Samuel Slávik <samuel.slavik@protonmail.com>
2025-08-23 20:37:46 +0200

    W PLG 8 create plugin for form elements
    * Add new plugin @musica-sacra/forms
    
    * Add changeset
    
    * Fix missing default props from html elements
    
    * Fix missing default props from html elements

--- packages/forms/src/components/input/Input.tsx
+++ packages/forms/src/components/input/Input.tsx
@@ -1,11 +1,12 @@
 import { useBem } from '@musica-sacra/hooks';
+import { InputHTMLAttributes } from 'react';
 
-type InputProps = {
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     className?: string;
 };
 
-export function Input({ className = '' }: InputProps) {
+export function Input({ className = '', ...props }: InputProps) {
     const { bem, base } = useBem('ms-input');
 
-    return <input className={bem(base, className)} />;
+    return <input className={bem(base, className)} {...props} />;
 }
