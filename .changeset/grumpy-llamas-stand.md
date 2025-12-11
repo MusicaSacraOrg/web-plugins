839114378a28976315cd9286067b544aec687dd6
samuel <samuel.slavik@protonmail.com>
2025-12-11 14:06:05 +0100

    Add new helpers to stringify axios params and fix their types

---
+++ .changeset/grumpy-llamas-stand.md
@@ -0,0 +1,5 @@
+---
+"@musica-sacra/api": major
+---
+
+Fix type of getList method. Add helpers for stringifying params for axios requests.
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,3 +3,5 @@ export { useGetById } from './hooks/useGetById';
 
 export { AbstractService } from './service/AbstractService';
 export { BaseService } from './service/BaseService';
+
+export { stringifyArrayParam } from './helpers/helpers';
